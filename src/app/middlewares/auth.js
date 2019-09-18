ab2fe0d28d406449769068e79cec72cb8b60d2e3
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2019-09-18 13:56:47 -0300

    JWT Auth and Update users w. Autentication

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+  // desestruturação para usar apenas o token posicao 0 é bearer
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // decoded o token para verficiar se valeo o token e assim o autenticar
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid' });
+  }
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '77145b84634d8ee1c6027534b65f7c68',
+  expiresIn: '7d',
+};
